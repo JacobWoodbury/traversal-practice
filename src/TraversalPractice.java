2fff43021844422c46f2d4af8d381741613defaf
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-05-01 12:27:49 -0700

    Traversal practice passing

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -8,7 +8,10 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static void printOddNodes(Node<Integer> node) {
-
+    if(node == null) return;
+    printOddNodes(node.left);
+    printOddNodes(node.right);
+    if(node.value %2 != 0) System.out.println(node.value);
   }
 
   /**
@@ -20,7 +23,15 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static <T> void printNodesWithOneChild(Node<T> node) {
-    
+    if(node == null) return;
+    //do logic
+    if(node.right == null && node.left == null){
+      return;
+    } else if(node.right == null || node.left == null) System.out.println(node.value);
+
+    printNodesWithOneChild(node.left);
+    printNodesWithOneChild(node.right);
+
   }
 
     /**
@@ -32,7 +43,9 @@ public class TraversalPractice {
    * @return the sum 
    */
   public static int treeSum(Node<Integer> node) {
-    return 0;
+    if(node == null) return 0;
+
+    return node.value + treeSum(node.left) + treeSum(node.right);
   }
 
   /**
@@ -45,7 +58,17 @@ public class TraversalPractice {
    * @return the max value
    */
   public static int maxVal(Node<Integer> node) {
-    return 0;
+   if(node == null) return 0;
+   Integer lefty = maxVal(node.left);
+   Integer righty = maxVal(node.right);
+
+    if(lefty > node.value && lefty > righty){
+      return lefty;
+    }
+    if(righty > node.value){
+      return righty;
+    }
+    return node.value;
   }
 
   /**
@@ -58,7 +81,12 @@ public class TraversalPractice {
    * @return The number of levels in the tree
    */
   public static <T> int numLevels(Node<T> node) {
-    return 0;
+    if(node == null) return 0;
+   Integer lefty = numLevels(node.left);
+   Integer righty = numLevels(node.right);
+  if(lefty >= righty) return 1+lefty;
+  else return 1+righty;
+    
   }
 
   public static void main(String[] args) {
@@ -72,7 +100,7 @@ public class TraversalPractice {
 
      // Replace the below line to create a tree 
      // as represented in the diagram above
-     Node<Integer> smallTree = null;
+     Node<Integer> smallTree = new Node<Integer>(99, new Node<>(45, new Node<>(9, null , null), new Node<>(5, null, null)), new Node<>(82, null, new Node<>(16, null, null)));
 
 
     /*
